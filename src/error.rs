77dc2b63251c8e4e92b63b0b21bd468ec8d9b342
nordluma <maxnordlund.95@hotmail.com>
2024-02-26 22:43:33 +0200

    feat(error): define error type for sql errors

---
+++ src/error.rs
@@ -0,0 +1,26 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+pub enum ApiError {
+    SqlError(sqlx::Error),
+}
+
+impl From<sqlx::Error> for ApiError {
+    fn from(e: sqlx::Error) -> Self {
+        Self::SqlError(e)
+    }
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        match self {
+            ApiError::SqlError(e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("SQL ERROR: {}", e),
+            )
+                .into_response(),
+        }
+    }
+}
