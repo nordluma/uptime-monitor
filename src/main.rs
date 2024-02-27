70dff1c2c06b72cd8c05605894e789c95cbf5151
nordluma <maxnordlund.95@hotmail.com>
2024-02-27 20:24:05 +0200

    feat: add handlers to http server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use axum::{
-    response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, post},
     Router,
 };
 use dotenvy::dotenv;
-use uptime_monitor::database::AppState;
+
+use uptime_monitor::{
+    database::AppState,
+    domain::{create_website, delete_website, get_website_alias, get_websites},
+    pages::styles,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -17,13 +21,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .expect("Migrations failed");
 
-    let app = Router::new().route("/", get(hello_world)).with_state(db);
+    let app = Router::new()
+        .route("/", get(get_websites))
+        .route("/websites", post(create_website))
+        .route(
+            "/websites/:alias",
+            get(get_website_alias).delete(delete_website),
+        )
+        .route("/styles.css", get(styles))
+        .with_state(db);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
     axum::serve(listener, app).await?;
 
     Ok(())
-}
-
-async fn hello_world() -> impl IntoResponse {
-    Html("Hello world")
 }
